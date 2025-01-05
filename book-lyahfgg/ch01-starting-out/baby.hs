2fae20e3071cd683718749b674898ad153de56bd
Parambir Singh <parambirs@gmail.com>
2025-01-05 12:06:35 -0800

    lyahfgg ch 01 - starting out

--- book-lyahfgg/ch01-starting-out/baby.hs
+++ book-lyahfgg/ch01-starting-out/baby.hs
@@ -4,11 +4,12 @@ doubleMe x = x + x
 -- takes two numbers and multiplies each by two and then adds them together.
 doubleUs x y = doubleMe x + doubleMe y
 
+-- multiplies a number by 2 but only if that number is smaller than or equal to 100
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
 
-doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+doubleSmallNumber' x = if x > 100 then x else x*2
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
 
